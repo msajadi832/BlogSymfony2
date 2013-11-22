428e83a59c74f82ec48f2ab87fa072e1d40c0aff
Morteza <msajadi832@hotmail.com>
2013-11-22 19:59:34 +0330

    twigs comment in Article and content_readmore & homepage added

--- src/blogger/blogBundle/Controller/blogController.php
+++ src/blogger/blogBundle/Controller/blogController.php
@@ -8,17 +8,28 @@ class blogController extends Controller
 {
     public function blogAction($blog_name)
     {
-        return $this->render('bloggerblogBundle:Blog/Article:Article.html.twig',
-            array('blog_name' => $blog_name, "content_name" => "نظرات اخیر", "sidebar_name" => "نظرات اخیر"));
+        $recent_comment = array(array("name" => "علی", "comment" => "سلام"),array("name" => "حسین", "comment" => "بیا"),
+            array("name" => "رضا", "comment" => "بای"));
+
+        $articles = array(array("article_title" => "علی", "article_date" => "12345", "article_body" => "سلام", "article_comment_count" => 5),
+            array("article_title" => "کتاب", "article_date" => "234234234", "article_body" => "مسنبدخهب هخبنمتثص بخهتس یبمنت خ", "article_comment_count" => 10),
+            array("article_title" => "یه مقاله خوب", "article_date" => "56452", "article_body" => "حخصم نصتبح خ  سیتبح هب نتسیبحثصخه ", "article_comment_count" => 50));
+
+        return $this->render('bloggerblogBundle:Blog/homepage:homepage.html.twig',
+            array('blog_name' => $blog_name, "sidebar_name" => "نظرات اخیر","recent_comments" => $recent_comment,"articles" => $articles));
     }
     public function articleAction($blog_name,$article_name)
     {
         $recent_comment = array(array("name" => "علی", "comment" => "سلام"),array("name" => "حسین", "comment" => "بیا"),
             array("name" => "رضا", "comment" => "بای"));
 
+        $comment_article = array(array("name" => "علی", "date" => "12345", "comment" => "سلام"),array("name" => "حسین", "date" => "54321", "comment" => "بیا"),
+            array("name" => "رضا", "date" => "777777", "comment" => "بای"));
+
         return $this->render('bloggerblogBundle:Blog/Article:Article.html.twig',
-            array('blog_name' => $blog_name, "content_name" => $article_name,
+            array('blog_name' => $blog_name, "article_title" => $article_name,
                 "sidebar_name" => "نظرات اخیر","recent_comments" => $recent_comment,
-            "article_date" => "1392/2/17", "article_body" => "سلام این اولین مقاله من است."));
+            "article_date" => "1392/2/17", "article_body" => "سلام این اولین مقاله من است.",
+            "article_comments" => $comment_article));
     }
 }
