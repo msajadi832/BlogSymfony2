0d0be2822c1655cb474e13a15f82f1aa77ba35f1
Morteza <msajadi832@hotmail.com>
2013-12-27 12:38:04 +0330

    add read comment and article secure

--- src/blogger/blogBundle/Controller/blogController.php
+++ src/blogger/blogBundle/Controller/blogController.php
@@ -15,7 +15,7 @@ class blogController extends Controller
         $articlesa = $user->getArticles();
         $articles = array();
         foreach($articlesa as $singleArticle){
-            $articles[] = array("article_title" => $singleArticle->getTitle(), "article_date" => date_format($singleArticle->getPublishDate(),"Y-m-d"),
+            $articles[] = array("article_title" => $singleArticle->getTitle(),"article_address" => $singleArticle->getAddress(), "article_date" => date_format($singleArticle->getPublishDate(),"Y-m-d"),
                 "article_body" => $singleArticle->getBody(),"article_comment_count" => $singleArticle->getComments()->count());
         }
 
@@ -26,12 +26,13 @@ class blogController extends Controller
         }
 
         return $this->render('bloggerblogBundle:Blog/homepage:homepage.html.twig',
-            array('blog_name' => $user->getBlogName(), "sidebar_name" => "نظرات اخیر","recent_comments" => $recent_comment,"articles" => $articles));
+            array('blog_info' => array('name' => $user->getBlogName(), 'address'=> $user->getBlogAddress()), "sidebar_name" => "نظرات اخیر",
+                "recent_comments" => $recent_comment,"articles" => $articles));
     }
     public function articleAction($blog_name,$article_name)
     {
         $user = $this->getDoctrine()->getRepository('bloggerblogBundle:User') ->findOneBy(array('blogAddress' => $blog_name));
-        $article = $this->getDoctrine() ->getRepository('bloggerblogBundle:Article') ->findOneBy(array("user" => $user->getId(),""));
+        $article = $this->getDoctrine() ->getRepository('bloggerblogBundle:Article') ->findOneBy(array("user" => $user->getId(),"address" => $article_name));
         $comment_article = array();
         foreach($article->getComments() as $singleComment){
             $comment_article[] = array("name" => $singleComment->getName(), "date" => date_format($singleComment->getDate(),"Y-m-d"), "comment" => $singleComment->getComment());
@@ -42,7 +43,8 @@ class blogController extends Controller
 
 
         return $this->render('bloggerblogBundle:Blog/Article:Article.html.twig',
-            array('blog_name' => $user->getBlogName(), "article_title" => $article->getTitle(),
+            array('blog_info' => array('name' => $user->getBlogName(), 'address'=> $user->getBlogAddress()),
+                "article_title" => $article->getTitle(),
                 "sidebar_name" => "نظرات اخیر","recent_comments" => $recent_comment,
             "article_date" => date_format($article->getPublishDate(),"Y-m-d"), "article_body" => $article->getBody(),
             "article_comments" => $comment_article));
