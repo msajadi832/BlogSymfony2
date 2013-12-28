8208315ec0f4882dfd2e60fff3494f8ec201c6c4
Morteza <msajadi832@hotmail.com>
2013-12-28 20:37:47 +0330

    add link to comment

--- src/blogger/blogBundle/Controller/blogController.php
+++ src/blogger/blogBundle/Controller/blogController.php
@@ -15,19 +15,20 @@ class blogController extends Controller
         $articlesa = $user->getArticles();
         $articles = array();
         foreach($articlesa as $singleArticle){
-            $articles[] = array("article_title" => $singleArticle->getTitle(),"article_address" => $singleArticle->getAddress(), "article_date" => date_format($singleArticle->getPublishDate(),"Y-m-d"),
+            $articles[] = array("article_title" => $singleArticle->getTitle(),"article_address" => $singleArticle->getAddress(), "article_date" => $singleArticle->getPublishDate(),
                 "article_body" => $singleArticle->getBody(),"article_comment_count" => $singleArticle->getComments()->count());
         }
 
         $comments_doc = $this->getDoctrine()->getRepository('bloggerblogBundle:Comment')->findBy(array("user" => $user->getId()),array("date" => 'DESC'),10);
-        $recent_comment = array();
+        $recent_comments = array();
         foreach($comments_doc as $singleComment){
-            $recent_comment[] = array("article_id" => $singleComment->getArticle()->getAddress(),"id" =>$singleComment->getId(),"name" => $singleComment->getName(), "comment" => $singleComment->getComment());
+            $recent_comments[] = array("article_address" => $singleComment->getArticle()->getAddress(),"id" =>$singleComment->getId(),
+                "name" => $singleComment->getName(), "date" => $singleComment->getDate(), "comment" => $singleComment->getComment());
         }
 
         return $this->render('bloggerblogBundle:Blog/homepage:homepage.html.twig',
-            array('blog_info' => array('name' => $user->getBlogName(), 'address'=> $user->getBlogAddress()), "sidebar_name" => "نظرات اخیر",
-                "recent_comments" => $recent_comment,"articles" => $articles));
+            array('blog_info' => array('name' => $user->getBlogName(), 'address'=> $user->getBlogAddress()),
+                "sidebar_name" => "نظرات اخیر", "recent_comments" => $recent_comments,"articles" => $articles));
     }
     public function articleAction($blog_name,$article_name)
     {
@@ -37,18 +38,16 @@ class blogController extends Controller
         foreach($article->getComments() as $singleComment){
             $comment_article[] = array("id" =>$singleComment->getId(),"name" => $singleComment->getName(), "date" => date_format($singleComment->getDate(),"Y-m-d"), "comment" => $singleComment->getComment());
         }
-        $comments_doc = $this->getDoctrine()->getRepository('bloggerblogBundle:Comment')->findBy(array("user" => $user->getId()),array("date" => 'DESC'),10);
-        $recent_comment = array();
-        foreach($comments_doc as $singleComment){
-            $recent_comment[] = array("article_id" => $singleComment->getArticle()->getAddress(),"id" =>$singleComment->getId(),"name" => $singleComment->getName(), "comment" => $singleComment->getComment());
+        $article_doc = $this->getDoctrine()->getRepository('bloggerblogBundle:Article')->findBy(array("user" => $user->getId()),array("publishDate" => 'DESC'),10);
+        $recent_articles = array();
+        foreach($article_doc as $singleArticle){
+            $recent_articles[] = array("title" => $singleArticle->getTitle(), "address" => $singleArticle->getAddress());
         }
 
 
         return $this->render('bloggerblogBundle:Blog/Article:Article.html.twig',
             array('blog_info' => array('name' => $user->getBlogName(), 'address'=> $user->getBlogAddress()),
-                "article_title" => $article->getTitle(),
-                "sidebar_name" => "نظرات اخیر","recent_comments" => $recent_comment,
-            "article_date" => date_format($article->getPublishDate(),"Y-m-d"), "article_body" => $article->getBody(),
-            "article_comments" => $comment_article));
+                "article_info" => array("title" => $article->getTitle(), "date" => $article->getPublishDate(),"body" => $article->getBody(),"comments" => $comment_article),
+                "sidebar_name" => "مقالات اخیر","recent_articles" => $recent_articles));
     }
 }
