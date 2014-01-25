be50b246d7b7e8cc67ce79269f22985d6d4d7fcf
mojtaba.banaie@gmail.com <msajadi832@hotmail.com>
2014-01-26 00:44:34 +0330

    added pagination

--- src/blogger/blogBundle/Entity/User.php
+++ src/blogger/blogBundle/Entity/User.php
@@ -8,13 +8,16 @@
 
 namespace blogger\blogBundle\Entity;
 
+
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
+ * @UniqueEntity(fields="blogAddress", message="این آدرس قبلا استفاده شده است.")
  */
 class User extends BaseUser{
     /**
